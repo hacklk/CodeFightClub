95d32167cfc2552449455afd4eea095b30c71b06
Krisztian Hackl <info@hackl.hu>
2020-11-14 14:46:20 +0100

    Update GameInventory_v1_1.py
    'remove_from_inventory' is working and testing

--- GameInventory/GameInventory_v1_1.py
+++ GameInventory/GameInventory_v1_1.py
@@ -10,8 +10,11 @@ def add_to_inventory(inventory, added_items):   #add the contents of the added_i
                                                 #if same occures more, then dictionary values are incremented those amounts o time
 
 
-def remove_from_inventory(inventory, removed_items):
-    pass
+def remove_from_inventory(inventory, removed_items):        #remove the contents of the removed_items list from the inventory dictionary
+    for supply in removed_items:
+        inventory[supply] = inventory.get(supply, 0) - 1    #decrease by one at every mention -> leaves supplies with 0 volume
+    [inventory.pop(supply) for supply in [amount for amount in inventory if inventory[amount] < 1]]  #Remove less-than-one supplies by key
+                                                            #as nothing < 1 stays, will not display deducted items, not in original dict
 
 
 def print_table(inventory, order):
@@ -32,6 +35,7 @@ def export_inventory(inventory, filename):
 game_inventory = {'Star Wars':5, 'ToyStory':7, 'WowG':10, 'Office':8}
 non_inventory = {}
 added_items = ['Star Wars', 'ToyStory', 'HubalaBaba', 'Seventh', 'Star Wars']
+no_exist = ['rubber', 'boy', 'leToy']
 
 print('Game Inventory: ')
 display_inventory(game_inventory)
@@ -40,5 +44,15 @@ display_inventory(non_inventory)
 
 print("\nAdd these items: ", added_items, " to the inventory, so ")
 add_to_inventory(game_inventory, added_items)
+print("Inventory is now ", )
+display_inventory(game_inventory)
+
+print("\nRemove these items: ", added_items, " from the inventory, so ")
+remove_from_inventory(game_inventory, added_items)
+print("Inventory is now ", )
+display_inventory(game_inventory)
+
+print("\nRemove these items: ", no_exist, " from the inventory, so ")
+remove_from_inventory(game_inventory, no_exist)
 print("Inventory is now ", )
 display_inventory(game_inventory)
