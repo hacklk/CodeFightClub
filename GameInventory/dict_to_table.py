b5e34bb46883f2a7ece24afed7340f732e521582
codeamazone <eniggeme@gmail.com>
2020-11-17 18:31:58 +0100

    Update dict_to_table.py
    updated dict_to_table file

--- GameInventory/dict_to_table.py
+++ GameInventory/dict_to_table.py
@@ -1,6 +1,16 @@
-game_inventory = {'Star Wars': 5, 'ToyStory': 7, 'WowG': 10, 'Office': 8}
+def print_table(inventory, order=None):
 
-print("{:<10} {:<10}".format('item name', 'count'))
+    keys = [len(key) for key in inventory]
+    max_width_left = max(keys)
+    max_width_right = len('count')
+    left = 'item name'  # left column name
+    right = 'count'     # right column name
 
-for key, value in game_inventory.items():
-    print("{:<10} {:<10}".format(key, value))
+    print((max_width_left + max_width_right + 5) * '-')
+    print(f'{left:>{max_width_left}} | {right:>{max_width_right}}')
+    print((max_width_left + max_width_right + 5) * '-')
+    for key, value in inventory.items():
+        print(f'{key:>{max_width_left}} | {value:>{max_width_right}}')
+
+
+print_table({'Star Wars': 5, 'ToyStory': 7, 'WowG': 10, 'Office': 8})
